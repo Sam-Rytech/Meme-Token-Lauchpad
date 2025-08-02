362ff7ee4e62c1033e203395775e2327168b588d
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-03 00:59:15 +0100

    Added app wrapper

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import { WalletProvider } from '../context/WalletContext'
+import { TokenProvider } from '../context/TokenContext'
+import Layout from '../components/Layout/Layout'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WalletProvider>
+      <TokenProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </TokenProvider>
+    </WalletProvider>
+  )
+}
