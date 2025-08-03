01a06519ff5cb3fb20d054f0e6b55ab7fbbb8f48
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-03 02:47:19 +0100

    Fixing build error from _app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import './globals.css'
 import { WalletProvider } from '../context/WalletContext'
 import { TokenProvider } from '../context/TokenContext'
 import Layout from '../components/Layout/Layout'
